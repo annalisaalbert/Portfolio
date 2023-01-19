f376f58b7a9e63705d27d8cb2d286b2736625bf0
Annalisa <92473536+annalisa-2001@users.noreply.github.com>
2023-01-19 11:44:13 -0500

    Add files via upload

---
+++ Personal Portfolio/script.js
@@ -0,0 +1,32 @@
+function scrolll(){
+    var left=document.querySelector(".portfolios");
+    left.scrollBy(350,0)
+}
+function scrollr(){
+    var right=document.querySelector(".portfolios");
+    right.scrollBy(-350,0)
+}
+function scrollbi(){
+    var left=document.querySelector(".bi");
+    left.scrollBy(350,0)
+}
+function scrollbir(){
+    var right=document.querySelector(".bi");
+    right.scrollBy(-350,0)
+}
+function scrollsql(){
+    var left=document.querySelector(".sql-port");
+    left.scrollBy(350,0)
+}
+function scrollsqlr(){
+    var right=document.querySelector(".sql-port");
+    right.scrollBy(-350,0)
+}
+function scrollpyth(){
+    var left=document.querySelector(".python-port");
+    left.scrollBy(350,0)
+}
+function scrollpythr(){
+    var right=document.querySelector(".python-port");
+    right.scrollBy(-350,0)
+}
